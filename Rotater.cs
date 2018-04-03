c78a2133c6b766786c9a0181449e6291f9cfc4db
Fikih Muhamad <lp02781@gmail.com>
2018-04-03 20:51:51 +0700

    Add files via upload

---
+++ Rotater.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    // Update is called once per frame
+    public int angleX;
+    public int angleY;
+    public int angleZ;
+    void Update ()
+    {
+        transform.Rotate (new Vector3 (angleX, angleY, angleZ) * Time.deltaTime);
+	}
+}
